0863c1a60d9e7c8a3495717cd19265c4fa2c9d9e
KatherineOracle <katherine@rasc.co.za>
2022-08-27 19:09:22 +0200

    update package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const PORT = process.env.PORT || 5000;
 const express = require("express");
 const cors = require("cors");
-const path = require("path"); //for heroku
+
 const mongoose = require("mongoose");
 const routes = require("./routes.js");
 const app = express();
@@ -10,13 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname,
-  'client', 'build','index.html'));
-  });
-}
 
 // connect to Mongo when the app initializes
 const uri =
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path"); //for heroku
 
 const  { 
     postTask,
@@ -20,4 +21,11 @@ router.route('/:id')
     .put(updateTask)
     .delete(deleteTask)
 
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname,
+    'client', 'build','index.html'));
+    });
+    }    
 module.exports = router
